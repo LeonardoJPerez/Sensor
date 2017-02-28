4c8201c5da9081d559c50d0d7a328c53c753a57a
Leonardo Perez <find.leonardo@gmail.com>
2017-02-27 19:15:20 -0500

    Added python options to shell wrapper.

--- fag.py
+++ fag.py
@@ -1,9 +1,15 @@
-from sense_hat import SenseHat
+# -*- coding: utf-8 -*-
+""" Docstring example"""
 
-sense = SenseHat()
-sense.set_rotation(r=180)
+#from sense_hat import SenseHat
+
+#sense = SenseHat()
+#sense.set_rotation(r=180)
 # sense.show_message("Fags!", text_colour=[0, 255, 0])
 
 
-print round(sense.get_temperature_from_humidity(), 2)
-print round(sense.get_temperature_from_pressure(), 2)
+#print round(sense.get_temperature_from_humidity(), 2)
+#print round(sense.get_temperature_from_pressure(), 2)
+
+print round(35.5546557645, 2)
+print round(39.4847363, 2)
--- sense-test.js
+++ sense-test.js
@@ -1,9 +1,13 @@
 var PythonShell = require('python-shell');
- 
-setInterval(function(){
-	PythonShell.run('fag.py', function (err, results) {
+
+var options = {
+	mode: 'json'
+};
+
+setInterval(function () {
+	PythonShell.run('fag.py', options, function (err, results) {
 		if (err) throw err;
-		// results is an array consisting of messages collected during execution 
-	  	console.log('results: %j', results);
+		// Results is a JSON array.
+		console.log('results: %j', results);
 	});
-}, 5000);
+}, 60000);

b6a56e5ba09d44254aaa13a963ce71e278f125f8
Leonardo Perez <find.leonardo@gmail.com>
2017-02-27 22:57:12 -0500

    Fixed py code.

--- fag.py
+++ fag.py
@@ -4,15 +4,14 @@
 #from sense_hat import SenseHat
 
 #sense = SenseHat()
-#sense.set_rotation(r=180)
+# sense.set_rotation(r=180)
 # sense.show_message("Fags!", text_colour=[0, 255, 0])
 
+# print round(sense.get_temperature_from_humidity(), 2)
+# print round(sense.get_temperature_from_pressure(), 2)
 
-#print round(sense.get_temperature_from_humidity(), 2)
-#print round(sense.get_temperature_from_pressure(), 2)
+import random as rand
 
-import random
-
-print random.uniform(36, 40)
-print random.uniform(36, 40)
-print random.uniform(36, 40)
+print(rand.uniform(15, 40))
+print(rand.uniform(30, 40))
+print(rand.uniform(30, 40))
